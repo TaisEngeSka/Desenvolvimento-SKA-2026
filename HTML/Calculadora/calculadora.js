6bd72876d03b239e5a475ef6381e4658603a0943
tais <tais.enge.ska@gmail.com>
2026-04-13 22:23:14 -0300

    hkhkjh

---
+++ HTML/Calculadora/calculadora.js
@@ -0,0 +1,14 @@
+function adicionar (valor){
+    let resultado = document.getElementById ("resultado");
+    resultado.value += valor;
+} 
+
+function limpar (){
+    let resultado = document.getElementById ("resultado");
+    resultado.value = "";
+}
+
+function calcular (){
+    let resultado = document.getElementById ("resultado");
+    resultado.value = eval (resultado.value);
+}
